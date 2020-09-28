ab4a1baaa70dd5e220217435d929ec2e2622466a
Leslie Rodriguez <rleslie1015@gmail.com>
2020-09-28 15:45:25 -0500

    starting cs35

--- hashtable/notes/notes1.py
+++ hashtable/notes/notes1.py
@@ -6,10 +6,11 @@ def my_hash(s):
     return total
 
 my_array = [None] * 8
+# You have to use the modulus operator ( % ) to get the division remainder 
+# to make sure the index is in range you would divide by the size of the hashtable
 hash_index = my_hash("hello world") % 8
 # print(my_hash("hello world")) # 1116
 
 print(f'my_arrray {my_array}')
 print(f'hash index {hash_index}')
-
 
